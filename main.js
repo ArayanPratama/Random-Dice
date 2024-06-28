3b5a796fc4e6b06a07deb791846de13f49c0bf47
ArayanPratama <arayanpratama265@gmail.com>
2024-06-28 09:59:44 +0700

    new

---
+++ main.js
@@ -0,0 +1,30 @@
+// Array gambar yang ingin dipilih secara acak
+const daftarGambar = [
+    "dice1.png",
+    "dice2.png",
+    "dice3.png",
+    "dice4.png",
+    "dice5.png",
+    "dice6.png",
+  ];
+  
+  function ambilGambarSecaraAcak() {
+    const hasil = Math.floor(Math.random() * daftarGambar.length);
+    return [daftarGambar[hasil],hasil];
+  }
+    const kembali1 = ambilGambarSecaraAcak();
+    const kembali2 = ambilGambarSecaraAcak();
+    document.querySelector(".img1").setAttribute("src", "./images/"+ kembali1[0]);
+    document.querySelector(".img2").setAttribute("src", "./images/"+ kembali2[0]);
+    
+    function siapaYangmenang (){
+     if ((kembali1[1]+1) - (kembali2[1]+1) > 0 ){
+     console.log("image1 win")
+    }else if((kembali1[1]+1) - (kembali2[1]+1) < 0 ){
+      console.log("image2 win")
+    }else{
+      console.log("draw")
+    }
+
+  }
+  siapaYangmenang();
